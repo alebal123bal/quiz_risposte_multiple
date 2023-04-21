a357cc7ed29fcf96cd283221b7041d422d652292
Alessandro Balzan <balzanalessandro2001@gmail.com>
2023-04-21 15:09:57 +0200

    Finish() main activity when showing results view to prevent illegal increment of score

--- app/src/main/java/com/example/quiz_risposte_multiple/LastActivity.kt
+++ app/src/main/java/com/example/quiz_risposte_multiple/LastActivity.kt
@@ -1,8 +1,8 @@
-package com.example.quiz_risposte_multiple;
+package com.example.quiz_risposte_multiple
 
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
 
 class LastActivity : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?){
